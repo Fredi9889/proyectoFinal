45790f85d615f12cd59c9f498c399fc5ce8b8e78
alfredo <email>
2020-06-05 19:11:31 +0200

    Create getActividades.php

---
+++ Proyecto final/sesionEncargado/getActividades.php
@@ -0,0 +1,23 @@
+<?php
+// Configuración BASE DE DATOS MYSQL
+$servidor  = "localhost";
+$basedatos = "eventos";
+$usuario   = "root";
+$password  = "";
+
+// Creamos la conexión al servidor.
+$conexion = mysqli_connect($servidor, $usuario, $password,$basedatos) or die(mysqli_error($conexion));
+mysqli_set_charset($conexion,"utf8");
+mysqli_query($conexion,"utf8");
+
+$sql = "SELECT a.idAct, a.nombre as nombreAct, t.nombre, a.lugar, a.fecha, a.hora FROM actividad a INNER join tipoactividad t on t.idTipo = a.idTipo";
+$resultado = mysqli_query($conexion,$sql);
+
+while($fila = mysqli_fetch_assoc($resultado)){
+    $datos[] = $fila;
+}
+echo json_encode($datos);
+
+mysqli_close($conexion);
+
+?>
