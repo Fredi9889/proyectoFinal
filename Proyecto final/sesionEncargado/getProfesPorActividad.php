a09453354afb1205a60e6c47c658d67f2600579d
alfredo <email>
2020-06-08 11:35:39 +0200

    Update getProfesPorActividad.php

--- Proyecto final/sesionEncargado/getProfesPorActividad.php
+++ Proyecto final/sesionEncargado/getProfesPorActividad.php
@@ -14,6 +14,7 @@ mysqli_set_charset($conexion,"utf8");
 // Consulta SQL para obtener los datos
 $sql = "SELECT a.idAct, a.nombre as nombreAct, i.confirmado, p.dni, p.nombre as nombreProf , p.apellidos, p.mail, p.colegio, c.nombre as nombreCuerpo from actividad a INNER JOIN inscripcion i on a.idAct = i.idAct INNER JOIN profesor p on i.dni = p.dni INNER JOIN cuerpoprofesor c on c.idCuerpo = p.idCuerpo where a.idAct = '".$idAct."'";
 $resultados = mysqli_query($conexion,$sql) or die(mysqli_error($conexion));
+$arrayDatos = array();
 while($fila = mysqli_fetch_assoc($resultados)){
     $arrayDatos[] = $fila;
 }
