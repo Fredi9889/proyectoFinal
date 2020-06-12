50eb338c33de10bffb38808b03c75a6881c7a166
alfredo <email>
2020-06-12 12:17:03 +0200

    Update getCuerpo.php

--- Proyecto final/registroProfe/getCuerpo.php
+++ Proyecto final/registroProfe/getCuerpo.php
@@ -1,17 +1,17 @@
 <?php
 
 // Configuración BASE DE DATOS MYSQL
-$servidor  = "localhost";
-$basedatos = "eventos";
-$usuario   = "root";
-$password  = "";
+$servidor  = "sql103.epizy.com";
+$basedatos = "epiz_25994339_eventos";
+$usuario   = "epiz_25994339";
+$password  = "05JSwZTZooLLw";
 
 // Creamos la conexión al servidor.
 $conexion = mysqli_connect($servidor, $usuario, $password,$basedatos) or die(mysqli_error($conexion));
 mysqli_set_charset($conexion,"utf8");
 
 // Consulta SQL para obtener los datos
-$sql = "select idCuerpo, nombre from cuerpoProfesor";
+$sql = "select idCuerpo, nombre from cuerpoprofesor";
 $resultados = mysqli_query($conexion,$sql) or die(mysqli_error($conexion));
 $arrayDatos = Array();
 while($fila = mysqli_fetch_assoc($resultados)){
